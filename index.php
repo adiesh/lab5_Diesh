1a04059c4a870461bf03773fa82533a1cd566d67
Andrew Diesh <adiesh@csumb.edu>
2017-10-20 02:01:29 +0000

    committing lab5

---
+++ index.php
@@ -0,0 +1,102 @@
+<?php
+
+   include 'information.php'; 
+   
+   
+   function displayListOfDevices() {
+       
+       $sql = "SELECT * from device WHERE 1"; // another way to select all attributes. 1: boolean, true.
+       $namedParameters = array(); 
+       
+       if (isset($_GET['submit'])) { // check if the user did a search. 
+            // now we check if any of the forms were filled out. 
+           if (!empty($_GET['device-name'])) {
+               // construct our SQL query accordingly.
+               $sql .=  " AND deviceName LIKE :deviceName"; 
+               $namedParameters[":deviceName"] = "%" . $_GET['device-name'] . "%"; // if a user enters a set of characters, it should give you a list containing those letters (ip: iPad, iPhone, etc)
+           }
+           
+           if (!empty($_GET['device-type'])) {
+               // construct our SQL query accordingly.
+               $sql .=   " AND deviceType = '". $_GET['device-type'] . "'"; 
+           }
+           
+           if (isset($_GET['available'])) {
+               // construct our SQL query accordingly.
+               $sql .=   " AND status = 'available'"; 
+           }
+           
+           if (isset($_GET['order-by'])) {
+               // construct our SQL query accordingly.
+               $sql .=   " ORDER BY ". $_GET['order-by']; 
+           }
+           
+           
+       }
+       $dbConn = checkDatabaseConnection(); 
+   
+       
+       $statement = $dbConn->prepare($sql);
+       $statement->execute($namedParameters);
+        
+       $records = $statement->fetchAll(); 
+        
+       foreach ($records as $record) {
+            echo $record["deviceName"]." ".$record["deviceType"]." ".$record["price"]." ".$record["status"]."<br>"; 
+       }
+   }
+   
+   function getDeviceTypes() {
+        $dbConn = checkDatabaseConnection();  // get the database connection.
+        $sql = 'SELECT DISTINCT(deviceType)  FROM device;'; 
+        $statement = $dbConn->prepare($sql);
+        $statement->execute();
+        
+        $records = $statement->fetchAll(); 
+        
+        foreach ($records as $record) {
+            echo "<option value='". $record['deviceType']. "'>". $record['deviceType']. "</option>"; 
+        }
+   }
+  
+  ?>
+  
+  
+  <html>
+      <head>
+          <title>Checkout Tech Devices</title>
+          <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+          <style>
+          @import url("./CSS/styles.css"); 
+          </style>
+      </head>
+      <body>
+          <header>
+              	<h1>Choose a device: </h1>
+          </header>
+          <form>
+               <strong><i>Device Name: </i></strong><input type="text" name="device-name">
+               <br>
+               <br>
+               <strong><i>Device Type: </i></strong> 
+               <select name="device-type">
+                     <option value=""></option>
+                     <?=getDeviceTypes()?>
+               </select>
+               <br>
+               <br>
+               <input type="checkbox" name="available"> <strong><i>Available: </i></strong>
+               <br>
+               <br>
+               <input type="radio" name="order-by" value="price"> <strong><i>Order By Price: </i></strong>
+               <br>
+               <br>
+               <input type="radio" name="order-by" value="deviceName"> <strong><i>Order By Name: </i></strong>
+               <br>
+               <br>
+               <input type="submit" value="Search" name="submit">
+          </form>
+          
+          <?=displayListOfDevices()?> <!-- calling displayListOfDevices(). Used to display all devices within devices schema
+      </body>
+  </html>
---
+++ information.php
@@ -0,0 +1,23 @@
+<?php
+    // A simple web site in Cloud9 that runs through Apache
+    // Press the 'Run' button on the top to start the web server,
+    // then click the URL that is emitted to the Output tab of the console
+function checkDatabaseConnection() {
+    $servername = "us-cdbr-iron-east-05.cleardb.net";
+    $username = "b5ba02fc3ba351";
+    $password = "3f870355";
+    $dbname = "heroku_860455424cb7b6b";
+            
+    // Create connection
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username, $password);
+    $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $conn;
+}
+          
+    
+?>
+
+        
+            
+           
+  
